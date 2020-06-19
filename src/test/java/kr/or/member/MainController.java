b5653145ff5645a5466eaae3d562bc50ad8fac00
hu-408 <hu-408@hu-408-PC>
2020-06-19 17:36:26 +0900

    hsql, mysql, jdbc 커넥션

--- src/test/java/kr/or/member/MainController.java
+++ src/test/java/kr/or/member/MainController.java
@@ -14,25 +14,25 @@ import kr.or.member.MemberVO;
 
 public class MainController {
 
-	private static final String TIME_SERVER = "pool.ntp.org";
+	private static final String TIME_SERVER = "pool.ntp.org";//MainControler 클래스 상수변수 선언
 
 	public static void main(String[] args) throws IOException {
-		System.out.println("클래스형 변수 - 사용자 정의 자료형 소스");
+		/*System.out.println("클래스형 변수 - 사용자 정의 자료형 소스");*/
 		//MemberVO 클래스를 실행하기 위해 new연산자 사용
 		MemberVO m1 = new MemberVO();
 		//setter 메소드를 이용해 해당 인스턴스 변수(field)에 값을 넣는다
-		m1.setName("가로쉬");
+		m1.setName("이태희");
 		m1.setAge(45);
 		m1.setPhoneNum("000-0000-0000");
 		//마찬가지로 다른 MemberVOS객체(instance)를 생성해서 값을 넣는다
 		
 		MemberVO m2 = new MemberVO();
-		m2.setName("티란데");
+		m2.setName("이태희");
 		m2.setAge(100);
 		m2.setPhoneNum("111-1111-1111");
 		
 		MemberVO m3 = new MemberVO();
-		m3.setName("안두인");
+		m3.setName("이태희");
 		m3.setAge(3);
 		m3.setPhoneNum("222-2222-2222");
 		
@@ -57,11 +57,12 @@ public class MainController {
 		} catch (SocketException e) {
 			System.out.println("open메서드 실행 오류");
 			e.printStackTrace();
-		} finally {
+		}finally{
 			System.out.println("이 부분은 성공/오류에 상관없이 항상 실행됩니다");
 		}
 		InetAddress address = InetAddress.getByName(TIME_SERVER);
 	    TimeInfo timeInfo = timeClient.getTime(address);
+	    
 	    //서버로부터 시간 가져오는 코드
 	    long returnTime = timeInfo.getMessage().getTransmitTimeStamp().getTime();
 	    Date date = new Date(returnTime);
--- src/test/java/kr/or/member/MemberService.java
+++ src/test/java/kr/or/member/MemberService.java
@@ -8,7 +8,10 @@ public class MemberService {
 		public void printMembers(MemberVO[] members) {
 			//향상된 for문(loop문) 사용(형태 = 단일값: 전체값)
 			for(MemberVO m : members) {
-				System.out.println("이름은: " + m.getName() + " | 나이는: " + m.getAge() + " | 전화번호는: " + m.getPhoneNum());
+				System.out.println("-------------");
+				System.out.println(m.getName());
+				System.out.println(m.getAge() + "세");
+				System.out.println(m.getPhoneNum());
 			}
 		}
 }
