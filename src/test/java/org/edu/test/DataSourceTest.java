80df2374d4c7674e4d04b5966a733d9c2bed91f9
hu-408 <hu-408@hu-408-PC>
2020-06-22 14:59:23 +0900

    통합구현 - HRD넷 구직자훈련과정 API 구현 실습 마무리

--- src/test/java/org/edu/test/DataSourceTest.java
+++ src/test/java/org/edu/test/DataSourceTest.java
@@ -12,18 +12,16 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-//locations으로 불러오도록 처리
 @ContextConfiguration(locations ={"file:src/main/webapp/WEB-INF/spring/**/*.xml"})
 @WebAppConfiguration
-
 public class DataSourceTest {
+
 	@Inject
 	private DataSource ds;
 	
 	@Test
-	public void testConnection()throws Exception{
+	public void testConnection() throws Exception {
 		Connection con = ds.getConnection();
-		System.out.println("데이터베이스 커넥션 성공:" + con);		
+		System.out.println("데이터베이스 커넥션 성공 : " + con);
 	}
-	
 }
